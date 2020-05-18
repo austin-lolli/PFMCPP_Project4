0022ab7e8d48af8775eda8bb3f104e2db16a1c26
Austin Lolli <austinlolli94@gmail.com>
2020-05-18 06:05:41 +0000

    Project 4 Part 7 Assignment submission

--- main.cpp
+++ main.cpp
@@ -297,22 +297,6 @@ int main()
     Numeric<int> it( 34 );
     Numeric<double> pi( 3.14 );
 
-/*
-    std::cout << "The result of FloatType^4 divided by IntType is: " << ft.multiply( ft ).multiply( ft ).divide( it ) << std::endl;
-    std::cout << "The result of DoubleType times 3 plus IntType is : " << dt.multiply( 3 ).add( it ) << std::endl;
-    std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it.divide( static_cast<int>(pi) ).multiply( static_cast<int>(dt) ).subtract( static_cast<int>(ft) ) << std::endl;
-    std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
-    std::cout << it.multiply(it).divide(0).divide(0.0f).divide(0.0)<<std::endl;
-    
-    std::cout << "FloatType x IntType  =  " << it.multiply( static_cast<int>(ft) ) << std::endl;
-    std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it.add( static_cast<int>(dt) ).add( static_cast<int>(ft) ).multiply( 24 ) << std::endl;
-    
-    std::cout << std::endl;
-    std::cout << std::endl;
-
- */
- //temp commment out for testing 
-
     Numeric<float> powFloat( 2.4f );
     Numeric<double> powDub( 12.25 );
     Numeric<int> powInt( 2 );
